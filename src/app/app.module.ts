2285512f7e0d52c7553d8ac483fd084590ee0cdc
Rodrigo Bruno de Oliveira <Rodrigo@MBP-de-Macbook.lan>
2019-03-14 21:56:44 -0300

    implementacao da primeira secao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule
